ae79a0adad46f40eb0ae9af9ee32ef44db7edc27
Tian Zhao <ztian1994@hotmail.com>
2020-04-24 23:33:24 -0700

    Add CharReplacement function.

--- c_program/c_program/main.c
+++ c_program/c_program/main.c
@@ -112,7 +112,23 @@ void TrimString() {
 	}
 }
 
+/* Replace tab by \t, backspace by \b, backslash by \\ */
+void CharReplacement() {
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			printf("\\t");
+		} else if (c == '\b') {
+			printf("\\b");
+		} else if (c == '\\') {
+			printf("\\\\");
+		} else {
+			putchar(c);
+		}
+	}
+}
 
 main() {
-	TrimString();
+	CharReplacement();
 }
